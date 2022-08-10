e2a832b2f46acf7e98495b2d01bfd65006f59a30
Milena Nedilska <nedilska.milena@gmail.com>
2022-08-10 11:20:20 +0300

    header, footer

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,8 @@ import * as flsFunctions from "./modules/functions.js"
 import "./modules/menu.js"
 
 flsFunctions.isWebp();
+
+let btn = document.getElementById('menu');
+ btn.addEventListener('click',()=>{
+    console.log('clickdsadasdas');
+ })
