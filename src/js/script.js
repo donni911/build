784a52b1447c6f87c6882056dbfaaf924eb23b7d
Bohdan200021 <bogdan.bigun2000@gmail.com>
2022-08-19 12:33:55 +0300

    burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
-function openMenu() {
-	document.getElementById("sidebar").classList.toggle('active');
+// function openMenu() {
+// 	document.getElementById("sidebar").classList.toggle('active');
+// }
+// function openMenu() {
+// 	document.getElementById("sidebar").classList.toggle('active');
+// }
+function openBurger() {
+	let buttonOpen = document.getElementById('toggler');
+
 }
