e3f2203b627beb1af5383e2a52f0d2b62a17d144
Milena <nedilska.milena@gmail.com>
2022-08-22 17:11:41 +0300

    dropdown menu,recipe block

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ function watcher() {
 export { svgSprive }
 
 const fonts = gulp.series(otfToTtf, ttfTowoff, fontsStyle);
-const mainTasks = gulp.series( gulp.parallel(copy, html, scss, js, images))
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 // const mainTasks = gulp.series( gulp.parallel(copy, html, scss,  images))
 // постройка сценаріїв якшо змінюються файли
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
